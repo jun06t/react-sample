981c464fd3074761a9f0d91b276be3a751fd777b
Jumpei Tsuji <tsuji_jumpei@cyberagent.co.jp>
2016-04-16 12:12:25 +0900

    Separate each component

--- form/src/form.jsx
+++ form/src/form.jsx
@@ -1,56 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import FormAction from './action/formAction.jsx';
-import FormStore from './store/formStore.jsx';
-
-class FormApp extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = FormStore.getAll();
-  }
-  componentDidMount() {
-    FormStore.addChangeListener(() => {
-      this.setState(FormStore.getAll());
-    });
-  }
-  render() {
-    return (
-      <div className="testApp">
-        <FormSend />
-        <FormDisplay data={this.state.value} />
-      </div>
-    );
-  }
-}
-
-class FormSend extends React.Component {
-  send(e) {
-    e.preventDefault();
-    var testValue = ReactDOM.findDOMNode(this.refs.test_value).value.trim();
-    FormAction.act(testValue);
-    ReactDOM.findDOMNode(this.refs.test_value).value = "";
-    return;
-  }
-
-  render() {
-    return (
-      <form>
-        <input type="text" ref="test_value" />
-        <button onClick={this.send.bind(this)}>送信</button>
-      </form>
-    );
-  }
-}
-
-class FormDisplay extends React.Component {
-  render() {
-    var message = this.props.data;
-    return (
-      <div>{message}</div>
-    );
-  }
-}
+import FormApp from './component/app.jsx';
 
 ReactDOM.render(
   <FormApp />,
